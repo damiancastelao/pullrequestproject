a167d19b4e33d744f0480c6323251a38e6060918
Damian Nogueiras <damian@danielcastelao.org>
2025-01-24 08:52:36 +0100

    EXP2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Pull Request");
         System.out.println(" nueva caracteristica");
+        System.out.println("finalizada caracteristica");
     }
 }
